3698afdf891c9674779c68b1a7f213d4035d47f9
pawel-rozwoda <pawel202@protonmail.com>
2021-09-10 23:13:19 +0200

    deposition test prototype

--- tests/pro-rata-solana-example.ts
+++ tests/pro-rata-solana-example.ts
@@ -14,40 +14,67 @@ describe('pro-rata-solana-example', () => {
     await program.state.rpc.new({
       accounts: {
         authority: provider.wallet.publicKey,
-      },
+      }
     });
   });
 
   it('deposition', async () => {
 
-		await program.state.rpc.deposit({
+		await program.state.rpc.deposit(new anchor.BN(0),{
 			accounts: {
 				authority: provider.wallet.publicKey,
 			},
-		});
+		}, );
 		const state = await program.state.fetch();
     
-		console.log(state.count)
+		console.log(state)
+		await new Promise(resolve => setTimeout(resolve, 600)); // in ms  
 
-		await program.state.rpc.deposit({
+		await program.state.rpc.deposit(new anchor.BN(1), {
 			accounts: {
 				authority: provider.wallet.publicKey,
 			},
 		});
 		const state2nd = await program.state.fetch();
-    
-		console.log(state2nd.count)
+		
+		console.log(state2nd)
+		await new Promise(resolve => setTimeout(resolve, 600)); // in ms  
+
+		await program.state.rpc.deposit(new anchor.BN(2), {
+			accounts: {
+				authority: provider.wallet.publicKey,
+			},
+		});
+		const state3nd = await program.state.fetch();
+		
+		console.log(state3nd)
+
+		await new Promise(resolve => setTimeout(resolve, 600)); // in ms  
+
+		await program.state.rpc.deposit(new anchor.BN(3), {
+			accounts: {
+				authority: provider.wallet.publicKey,
+			},
+		});
+		const state4th = await program.state.fetch();
+		
+		console.log(state4th)
+
+		await new Promise(resolve => setTimeout(resolve, 600)); // in ms  
+
+		await program.state.rpc.deposit(new anchor.BN(4), {
+			accounts: {
+				authority: provider.wallet.publicKey,
+			},
+		});
+		const state5th = await program.state.fetch();
+		
+		console.log(state5th)
+		//console.log(typeof(state2nd.share))
+		//console.log(state2nd.share)
+		////console.log(Object.keys(state2nd.share))
+		//console.log(state2nd.share.BN)
   });
 
-  //it('2nd deposition', async () => {
-		//await program.state.rpc.deposit({
-			//accounts: {
-				//authority: provider.wallet.publicKey,
-			//},
-		//});
-		//const state = await program.state.fetch();
-    
-		//console.log(state.count)
-  //});
 
 });
